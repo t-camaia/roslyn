a0b6b3793cf3a8954f7691089f422648db015857
Oleg Tkachenko <olegtk@microsoft.com>
2017-12-11 10:44:48 -0800

    add new files

---
+++ src/EditorFeatures/Core/Shared/Utilities/ProgressTrackerAdapter.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.Shared.Utilities;
+
+namespace Microsoft.CodeAnalysis.Editor.Shared.Utilities
+{
+    internal class ProgressTrackerAdapter : IProgressTracker
+    {
+        private readonly VisualStudio.Utilities.IProgressTracker _platformProgressTracker;
+
+        public ProgressTrackerAdapter(VisualStudio.Utilities.IProgressTracker platformprogressTracker)
+        {
+            Requires.NotNull(platformprogressTracker, nameof(platformprogressTracker));
+            _platformProgressTracker = platformprogressTracker;
+        }
+
+        public int CompletedItems => _platformProgressTracker.CompletedItems;
+
+        public int TotalItems => _platformProgressTracker.TotalItems;
+
+        public void AddItems(int count)
+        {
+            _platformProgressTracker.AddItems(count);
+        }
+
+        public void Clear()
+        {
+            _platformProgressTracker.Clear();
+        }
+
+        public void ItemCompleted()
+        {
+            _platformProgressTracker.ItemCompleted();
+        }
+    }
+}
---
+++ src/EditorFeatures/Core/Shared/Utilities/WaitContextAdapter.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading;
+using Microsoft.CodeAnalysis.Editor.Host;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Microsoft.CodeAnalysis.Editor.Shared.Utilities
+{
+    internal class WaitContextAdapter : IWaitContext
+    {
+        private readonly IWaitableUIOperationScope _platformWaitScope;
+
+        public WaitContextAdapter(IWaitableUIOperationScope waitableScope)
+        {
+            Requires.NotNull(waitableScope, nameof(waitableScope));
+            _platformWaitScope = waitableScope;
+        }
+
+        public CancellationToken CancellationToken => _platformWaitScope.Context.CancellationToken;
+
+        public bool AllowCancel
+        {
+            get => _platformWaitScope.AllowCancellation;
+            set => _platformWaitScope.AllowCancellation = value;
+        }
+
+        public string Message
+        {
+            get => _platformWaitScope.Description;
+            set => _platformWaitScope.Description = value;
+        }
+
+        public CodeAnalysis.Shared.Utilities.IProgressTracker ProgressTracker
+        {
+            get
+            {
+                return _platformWaitScope.ProgressTracker != null ? new ProgressTrackerAdapter(_platformWaitScope.ProgressTracker) : null;
+            }
+        }
+
+        public void Dispose()
+        {
+            _platformWaitScope.Dispose();
+        }
+    }
+}
